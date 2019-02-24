4311f4d518ea65b1dfd7c49b5594713b3411f0a0
Jhonata Emanuel <xxxonata@gmail.com>
2019-02-24 16:21:08 -0300

    Tratando múltiplos eventos com split()

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,7 +1,7 @@
 class CalcController {
     
     constructor(){
-        this._locale = 'pt-BR'
+        this._locale = 'pt-BR';
         this._displayCalcEl = document.querySelector("#display");
         this._dateEl = document.querySelector("#data");
         this._timeEl = document.querySelector("#hora");
@@ -18,14 +18,24 @@ class CalcController {
         }, 1000);
     }
 
+    addEventListenerAll(element, events, fn){
+        events.split(' ').forEach(event =>{
+            element.addEventListener(event, fn, false);
+        });
+    }
+
     initButtonsEvents(){
         let buttons = document.querySelectorAll("#buttons > g, #parts > g");
 
-        buttons.forEach((btn, index )=> {
-            btn.addEventListener('click', e =>{
+        buttons.forEach((btn, index) => {
+            this.addEventListenerAll(btn, 'click drag', e =>{
                 console.log(btn.className.baseVal.replace("btn-", ""));
             });
-        })
+
+            this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
+                btn.style.cursor = "pointer";
+            });
+        });
     }
 
     setDisplayDateTime(){
