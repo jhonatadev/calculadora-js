8fd1924c30c7f96a6bad8451686cc07ed7d09c0c
jhonatadev <xxxonata@gmail.com>
2019-07-28 18:00:43 -0300

    Data e Hora && Refatoração

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,31 +1,53 @@
 class CalcController {
 	constructor() {
-		this._displayCalc = "0"
+		this._locale = 'pt-BR'
+		this._displayCalcEl = document.querySelector('#display')
+		this._dateEl = document.querySelector('#data')
+		this._timeEl = document.querySelector('#hora')
 		this._currentDate
 		this.initialize()
 	}
 
 	initialize() {
-		let displayCalcEl = document.querySelector('#display')
-		let dateEl = document.querySelector('#data')
-		let timeEl = document.querySelector('#hora')
+		this.setDisplayDateTime()
+		
+		setInterval(() => {
+			this.setDisplayDateTime()
+		}, 1000)
+	}
+	
+	setDisplayDateTime() {
+		this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
+			day: "2-digit"	, month: "long", year: "numeric"
+		})
+		this.displayTime = this.currentDate.toLocaleTimeString(this._locale)
+	}
 
-		displayCalcEl.innerHTML = '465456'
-		dateEl.innerHTML = '28/07/2019'
-		timeEl.innerHTML = '23:59'
+	get displayTime() {
+		return this._timeEl.innerHTML
+	}
+	set displayTime(value) {
+		return this._timeEl.innerHTML = value
+	}
+	
+	get displayDate() {
+		return this._dateEl.innerHTML
+	}
+	set displayDate(value) {
+		return this._dateEl.innerHTML = value
 	}
 
 	get displayCalc() {
-		return this._displayCalc
+		return this._displayCalcEl.innerHTML
 	}
-	set displayCalc(valor) {
-		this._displayCalc = valor
+	set displayCalc(value) {
+		this._displayCalcEl.innerHTML = value
 	}
 
 	get currentDate() {
-		return this._currentDate
+		return new Date()
 	}
-	set currentDate(valor) {
-		this._currentDate = valor
+	set currentDate(value) {
+		this._currentDate = value
 	}
 }
