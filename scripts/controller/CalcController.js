6ffbfeeac788c4fd6e2ec4a4cdd6494614cad5d9
jhonatadev <xxxonata@gmail.com>
2019-07-28 15:36:20 -0300

    Método Construtor e Encapsulamento

---
+++ scripts/controller/CalcController.js
@@ -0,0 +1,20 @@
+class CalcController {
+	constructor() {
+		this._displayCalc = "0"
+		this._dataAtual
+	}
+
+	get displayCalc() {
+		return this._displayCalc
+	}
+	set displayCalc(valor) {
+		this._displayCalc = valor
+	}
+
+	get dataAtual() {
+		return this._dataAtual
+	}
+	set dataAtual(data) {
+		this._dataAtual = data
+	}
+}
