e185be6a2e2538b1fab55f3c47ddb27ad23a9302
jhonatadev <xxxonata@gmail.com>
2019-07-28 18:27:22 -0300

    Adicionando Evento Click

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -6,6 +6,7 @@ class CalcController {
 		this._timeEl = document.querySelector('#hora')
 		this._currentDate
 		this.initialize()
+		this.initButtonsEvents()
 	}
 
 	initialize() {
@@ -14,6 +15,17 @@ class CalcController {
 		setInterval(() => {
 			this.setDisplayDateTime()
 		}, 1000)
+	}
+
+	initButtonsEvents() {
+		let buttons = document.querySelectorAll('#buttons > g, #parts > g')
+
+		buttons.forEach((btn, index) => {
+			btn.addEventListener('click', e => {
+				console.log(btn.className.baseVal.replace('btn-', ''))
+			})
+		})
+
 	}
 	
 	setDisplayDateTime() {
