aab12fa9e1866fcc72e878eb5949f431a44402bb
jhonatadev <xxxonata@gmail.com>
2019-07-30 02:13:00 -0300

    Switch

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,5 +1,6 @@
 class CalcController {
 	constructor() {
+		this._operation = []
 		this._locale = 'pt-BR'
 		this._displayCalcEl = document.querySelector('#display')
 		this._dateEl = document.querySelector('#data')
@@ -23,12 +24,74 @@ class CalcController {
 		})
 	}
 
+	clearAll() {
+		this._operation = []
+	}
+
+	clearEntry() {
+		this._operation.pop()
+	}
+
+	addOperation(value) {
+		this._operation.push(value)
+		console.log(this._operation)
+	}
+
+	setError() {
+		this.displayCalc = 'Error'
+	}
+
+	execBtn(value) {
+		switch (value) { 
+			case 'ac':
+				this.clearAll()
+				break;
+			case 'ce':
+				this.clearEntry()
+				break;
+			case 'soma':
+				
+				break;
+			case 'subtracao':
+				
+				break;
+			case 'divisao':
+				
+				break;
+			case 'multiplicacao':
+				
+				break;
+			case 'percento':
+				
+				break;
+			case 'igual':
+				
+				break;
+			case '0':	
+			case '1':	
+			case '2':	
+			case '3':	
+			case '4':	
+			case '5':	
+			case '6':	
+			case '7':	
+			case '8':	
+			case '9':
+				this.addOperation(parseInt(value))
+				break;	
+			default:
+				this.setError()
+				break;
+		}
+	}
+
 	initButtonsEvents() {
 		let buttons = document.querySelectorAll('#buttons > g, #parts > g')
 
 		buttons.forEach((btn, index) => {
-			this.addEventListenerAll(btn,'click drag mouseover', e => {
-				console.log(btn.className.baseVal.replace('btn-', ''))
+			this.addEventListenerAll(btn,'click drag', e => {
+				let textBtn = btn.className.baseVal.replace('btn-', '')
+				this.execBtn(textBtn)
 			})
 
 			this.addEventListenerAll(btn, 'mouseover mouseup mousedown', e => {
