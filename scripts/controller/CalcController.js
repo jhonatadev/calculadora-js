fd8cd717362df4921bfc9a97c0245a31b5c7da4e
jhonatadev <xxxonata@gmail.com>
2019-08-20 23:53:13 -0300

    Tratar outros erros no eval() com Tray Catch

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -154,7 +154,13 @@ class CalcController {
   }
 
   getResult() {
-    return eval(this._operation.join(''))
+    try {
+      return eval(this._operation.join(''))
+    } catch(e) {
+      setTimeout(() => {
+        this.setError()
+      }, 1)
+    }
   }
 
   calc() {
